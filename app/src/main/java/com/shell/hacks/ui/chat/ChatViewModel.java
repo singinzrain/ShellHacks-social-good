d24d776b678dbe345391aa0d316bfdfcd6f3fc6e
samurai <1214737761@qq.com>
2021-09-25 15:02:38 -0700

    rename

--- app/src/main/java/com/shell/hacks/ui/chat/ChatViewModel.java
+++ app/src/main/java/com/shell/hacks/ui/chat/ChatViewModel.java
@@ -1,16 +1,16 @@
-package com.shell.hacks.ui.home;
+package com.shell.hacks.ui.chat;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class ChatViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public ChatViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is notifications fragment");
     }
 
     public LiveData<String> getText() {
--- app/src/main/java/com/shell/hacks/ui/help/HelpViewModel.java
+++ app/src/main/java/com/shell/hacks/ui/help/HelpViewModel.java
@@ -1,16 +1,16 @@
-package com.shell.hacks.ui.notifications;
+package com.shell.hacks.ui.help;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class HelpViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public HelpViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is home fragment");
     }
 
     public LiveData<String> getText() {
